6ad3d15a0da44736ed0450372b5b5bbf9e5913cb
Florian <florian.olivierandre@gmail.com>
2018-12-27 23:16:45 +0100

    add vue-material

---
+++ src/types/material.d.ts
@@ -0,0 +1,39 @@
+declare module 'vue-material/dist/components' {
+  import {PluginFunction} from 'vue'
+
+  export const MdApp           : PluginFunction<never>
+  export const MdAutocomplete  : PluginFunction<never>
+  export const MdAvatar        : PluginFunction<never>
+  export const MdBadge         : PluginFunction<never>
+  export const MdBottomBar     : PluginFunction<never>
+  export const MdButton        : PluginFunction<never>
+  export const MdCard          : PluginFunction<never>
+  export const MdCheckbox      : PluginFunction<never>
+  export const MdChips         : PluginFunction<never>
+  export const MdContent       : PluginFunction<never>
+  export const MdDatepicker    : PluginFunction<never>
+  export const MdDialog        : PluginFunction<never>
+  export const MdDivider       : PluginFunction<never>
+  export const MdDrawer        : PluginFunction<never>
+  export const MdElevation     : PluginFunction<never>
+  export const MdEmptyState    : PluginFunction<never>
+  export const MdField         : PluginFunction<never>
+  export const MdHighlightText : PluginFunction<never>
+  export const MdIcon          : PluginFunction<never>
+  export const MdImage         : PluginFunction<never>
+  export const MdLayout        : PluginFunction<never>
+  export const MdList          : PluginFunction<never>
+  export const MdMenu          : PluginFunction<never>
+  export const MdProgress      : PluginFunction<never>
+  export const MdRadio         : PluginFunction<never>
+  export const MdRipple        : PluginFunction<never>
+  export const MdSnackbar      : PluginFunction<never>
+  export const MdSpeedDial     : PluginFunction<never>
+  export const MdSteppers      : PluginFunction<never>
+  export const MdSubheader     : PluginFunction<never>
+  export const MdSwitch        : PluginFunction<never>
+  export const MdTable         : PluginFunction<never>
+  export const MdTabs          : PluginFunction<never>
+  export const MdToolbar       : PluginFunction<never>
+  export const MdTooltip       : PluginFunction<never>
+}
