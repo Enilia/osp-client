241409dc471bdea83c2950e5c41a01ba85604fd0
Florian <florian.olivierandre@gmail.com>
2018-12-30 18:38:28 +0100

    user is first in the room list + add client component

--- src/views/Room.vue
+++ src/views/Room.vue
@@ -2,28 +2,14 @@
   <div>
     <h2> #{{ room.id }} </h2>
 
-    <md-card v-for="client in room.clients" :key="client.id" md-theme="white-card" class="md-primary md-elevation-1 client-card">
-      <md-card-header>
-        <div class="md-title" v-if="client.nickname"> {{ client.nickname }}</div>
-        <div class="md-title" v-else>@{{ client.id }}</div>
-        <div class="md-subhead">@{{ client.id }}</div>
-      </md-card-header>
-      <md-card-content>
-
-        <md-card class="osp-card md-elevation-3">
-          <md-card-content>
-            ?
-          </md-card-content>
-        </md-card>
-
-      </md-card-content>
-    </md-card>
+    <osp-client v-for="client in roomClients" :key="client.id" :client="client">
+    </osp-client>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
-import { State, Action } from 'vuex-class';
+import { State, Action, Getter } from 'vuex-class';
 import { Room } from '@/interfaces/room.interface';
 import { User } from '@/interfaces/user.interface';
 
@@ -40,8 +26,8 @@ export default class RoomComponent extends Vue {
   @State('room')
   room!: Room
 
-  @State('user')
-  user!: User
+  @Getter('roomClientsUserFirst')
+  roomClients!: User[]
 
   @Action('leaveRoom')
   leaveRoom!: () => void
@@ -51,36 +37,4 @@ export default class RoomComponent extends Vue {
 </script>
 
 <style lang="scss">
-@import "~vue-material/dist/theme/engine";
-
-@include md-register-theme("white-card", (
-  primary: md-get-palette-color(grey, 900)
-));
-
-@import "~vue-material/dist/base/theme";
-@import "~vue-material/dist/components/MdCard/theme";
-
-.md-card.client-card {
-  width: 250px;
-  margin: 4px;
-  display: inline-block;
-  vertical-align: top;
-
-  .md-title {
-    text-overflow: ellipsis;
-    overflow: hidden;
-  }
-}
-
-.osp-card {
-  height: 200px;
-
-  .md-card-content {
-    display: flex;
-    height: 100%;
-    align-items: center;
-    justify-content: center;
-    font-size: 5em;
-  }
-}
 </style>
